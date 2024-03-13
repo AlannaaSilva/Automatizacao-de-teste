fdf3bf2c5a00d8457a2d107e367f4d57c9440d64
Alanna Silva <alannacostas55@gmail.com>
2024-03-13 16:14:28 -0300

    adionando novos testes para comportamento de erro

--- TesteAdicionarProjeto/teste2.py
+++ TesteAdicionarProjeto/teste2.py
@@ -1,9 +1,7 @@
 from time import sleep
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service  # Importa a classe Service
-caminho_foto = 'C:/Users/alann/OneDrive/Documentos/Automatização-de-teste/TesteAdicionarProjeto/imagens/imagem-teste.jpg' 
-
+from selenium.webdriver.chrome.service import Service
 
 servico = Service(ChromeDriverManager().install())  #Cria uma instância de Service
 navegador = webdriver.Chrome(service=servico)
@@ -29,10 +27,8 @@ navegador.find_element('xpath','//*[@id=":r3:"]').click()
 
 sleep(5)
 
-
-#Testar a funcionalidade de adicionar titulo
-navegador.find_element('xpath', '//input[@name="title" and @type="text"]').send_keys('Exemplo de título para teste')
-
+#Testar se o sistema impede a adição de um titulo incorreto com (menos de 4 caracteres).
+navegador.find_element('xpath', '//input[@name="title" and @type="text"]').send_keys('Olá')
 sleep(2)
 
 # Testar se  funcionalidade de adicionar tag
@@ -46,32 +42,18 @@ navegador.find_element('xpath', "//li[@role='option'][1]").click()
 
 sleep(2)
 
-# Testar se  funcionalidade de adicionar link
-navegador.find_element('xpath', '//input[@name="link" and @type="text"]').send_keys('www.behance.com.br')
+#Testar se o sistema impede a adição de um link incorreto com (menos de 4 caracteres).
+navegador.find_element('xpath', '//input[@name="link" and @type="text"]').send_keys('www')
 
 sleep(2)
 
-# Testar se  funcionalidade de adicionar descrição
-navegador.find_element('xpath', '//textarea[@id="outlined-multiline-static"]').send_keys('Esse é um exemplo de como a descrição deve ser!')
+# Testar se o sistema impede a adição de um descrição incorreto com (menos de 4 caracteres)
+navegador.find_element('xpath', '//textarea[@id="outlined-multiline-static"]').send_keys('des')
 
 sleep(2)
 
-
-# Testar se  funcionalidade de adicionar foto
-navegador.find_element('xpath', '//input[@id="fileInput" and @name="imagem" and @type="file"]').send_keys(caminho_foto)  
- 
-
-sleep(2)
-
-# Testar se  funcionalidade de visualizar publicação
-# navegador.find_element('xpath', '//p[contains(text(), "Visualizar publicação")]').click() 
-
-# sleep(2)
-
-# Testar se  funcionalidade de salvar publicação
 navegador.find_element('xpath',' //*[@id=":r6:"]').click() # salvar publicação
 
 sleep(10)
 
 navegador.quit()
-
